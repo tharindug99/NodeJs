9060d60c4cb0a4917c99a5531de9486708c577cf
Tharindu-99 <tngodage@std.appsc.sab.ac.lk>
2023-02-19 13:51:14 +0530

    Final commits for 19/02/2023

---
+++ Modules and Require/modules.js
@@ -0,0 +1,4 @@
+const xyz = require('./people');      ///Import a file (When neede to automatically load a file)
+
+console.log(xyz);      //This file is currently an empty object
+
--- Modules and Require/people.js
+++ Modules and Require/people.js
@@ -1,1 +1,5 @@
- 
+ const people = ['Tom','Jerry','Tweety','Pooh'];
+
+ console.log(people);
+
+ module.exports = 'Hello';  //This will be the ouput received by the importer file
