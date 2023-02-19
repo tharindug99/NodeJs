7c5716e4d82bc719bd139307f1edd1de2bb5e00d
Tharindu-99 <tngodage@std.appsc.sab.ac.lk>
2023-02-19 09:44:45 +0530

    Added People and Modules

--- global.js
+++ global.js
@@ -2,6 +2,15 @@
 
 
 
-setTimeout(() => {
-    console.log('this is the timeout that occurs after 3 seconds')
-}, 3000);
+// setTimeout(() => {       //This is only run once
+//     console.log('this is the timeout that occurs after 3 seconds');
+//     clearInterval(int);
+// }, 3000);
+
+// const int = setInterval(() => {
+//     console.log('this is in the interval');    
+//     clearInterval(int);
+// },1000);
+
+console.log(__dirname);       ///get directory details
+console.log(__filename);      ///get file details
