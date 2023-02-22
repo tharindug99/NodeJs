a31f71abbf67379636fced2c85bdacc87a24358e
Tharindu-99 <tngodage@std.appsc.sab.ac.lk>
2023-02-22 12:13:37 +0530

    modified:   server.js

--- server.js
+++ server.js
@@ -1,8 +1,29 @@
 const http = require('http');
 
 const server = http.createServer((req, res) =>{
-    console.log('request made');
-})
+    console.log('request made')
+    console.log(req.url, req.method);
+
+    ///set header content type
+ res.setHeader('Content-Type','text/html');
+
+//  res.write('<p>Hello Again !!</p>');
+//  res.write('<p>Hello Again !!</p>');
+//  res.end();
+
+let path ='./views/';
+switch (req.url) {
+    case value:
+        
+        break;
+
+    default:
+        break;
+}
+
+
+});
+
 
 server.listen(3000, 'localhost', ()=>{
     console.log('Listening on port 3000');
